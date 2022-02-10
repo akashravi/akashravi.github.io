f8070b3ac103c33565bf18dc2a38fa809b6841bd
Akash Ravi <akashkravi@gmail.com>
2022-02-10 13:47:12 -0500

    Nav improvements + code cleanup

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,11 +9,11 @@ var resumeLoaded = false;
 function loadResume() {
 	if (window.location.hash === '#work' && !resumeLoaded) {
 		$('iframe#resume_preview').attr('src', 'https://drive.google.com/file/d/1_mfqrYqnBg2KYWOGcoykjsqUkipdB4Nt/preview');
-		resumeLoaded = true; 
+		resumeLoaded = true;
 	}
 }
 
-(function($) {
+(function ($) {
 
 	var $window = $(window),
 		$body = $('body'),
@@ -23,201 +23,201 @@ function loadResume() {
 		$nav = $('#nav'), $nav_links = $nav.children('a');
 
 	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ '361px',   '736px'  ],
-			xsmall:  [ null,      '360px'  ]
-		});
+	breakpoints({
+		xlarge: ['1281px', '1680px'],
+		large: ['981px', '1280px'],
+		medium: ['737px', '980px'],
+		small: ['361px', '736px'],
+		xsmall: [null, '360px']
+	});
 
 	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+	$window.on('load', function () {
+		window.setTimeout(function () {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
 
 	// Nav.
-		$nav_links
-			.on('click', function(event) {
+	$nav_links
+		.on('click', function (event) {
 
-				var href = $(this).attr('href');
+			var href = $(this).attr('href');
 
-				// Not a panel link? Bail.
-					if (href.charAt(0) != '#'
-					||	$panels.filter(href).length == 0)
-						return;
+			// Not a panel link? Bail.
+			if (href.charAt(0) != '#'
+				|| $panels.filter(href).length == 0)
+				return;
 
-				// Prevent default.
-					event.preventDefault();
-					event.stopPropagation();
+			// Prevent default.
+			event.preventDefault();
+			event.stopPropagation();
 
-				// Change panels.
-					if (window.location.hash != href)
-						window.location.hash = href;
+			// Change panels.
+			if (window.location.hash != href)
+				window.location.hash = href;
 
-			});
+		});
 
 	// Panels.
 
-		// Initialize.
-			(function() {
+	// Initialize.
+	(function () {
 
-				var $panel, $link;
+		var $panel, $link;
 
-				// Get panel, link.
-					if (window.location.hash) {
+		// Get panel, link.
+		if (window.location.hash) {
 
-				 		$panel = $panels.filter(window.location.hash);
-						$link = $nav_links.filter('[href="' + window.location.hash + '"]');
-
-					}
-
-				// No panel/link? Default to first.
-					if (!$panel
-					||	$panel.length == 0) {
-
-						$panel = $panels.first();
-						$link = $nav_links.first();
-
-					}
-
-				// Deactivate all panels except this one.
-					$panels.not($panel)
-						.addClass('inactive')
-						.hide();
-
-				// Activate link.
-					$link
-						.addClass('active');
-
-				// Reset scroll.
-					$window.scrollTop(0);
-
-					loadResume();
-			})();
-
-		// Hashchange event.
-			$window.on('hashchange', function(event) {
-
-				var $panel, $link;
-
-				// Get panel, link.
-					if (window.location.hash) {
-
-				 		$panel = $panels.filter(window.location.hash);
-						$link = $nav_links.filter('[href="' + window.location.hash + '"]');
-
-						// No target panel? Bail.
-							if ($panel.length == 0)
-								return;
-
-					}
-
-				// No panel/link? Default to first.
-					else {
-
-						$panel = $panels.first();
-						$link = $nav_links.first();
-
-					}
-
-				// Deactivate all panels.
-					$panels.addClass('inactive');
-
-				// Deactivate all links.
-					$nav_links.removeClass('active');
-
-				// Activate target link.
-					$link.addClass('active');
-
-				// Set max/min height.
-					$main
-						.css('max-height', $main.height() + 'px')
-						.css('min-height', $main.height() + 'px');
-
-				// Delay.
-					setTimeout(function() {
-
-						// Hide all panels.
-							$panels.hide();
-
-						// Show target panel.
-							$panel.show();
-
-						// Set new max/min height.
-							$main
-								.css('max-height', $panel.outerHeight() + 'px')
-								.css('min-height', $panel.outerHeight() + 'px');
-
-						// Reset scroll.
-							$window.scrollTop(0);
-
-						// Delay.
-							window.setTimeout(function() {
-
-								// Activate target panel.
-									$panel.removeClass('inactive');
-
-								// Clear max/min height.
-									$main
-										.css('max-height', '')
-										.css('min-height', '');
-
-								// IE: Refresh.
-									$window.triggerHandler('--refresh');
-
-								// Unlock.
-									locked = false;
-
-							}, (breakpoints.active('small') ? 0 : 500));
-
-					}, 250);
-
-					loadResume();
-			});
-
-	// IE: Fixes.
-		if (browser.name == 'ie') {
-
-			// Fix min-height/flexbox.
-				$window.on('--refresh', function() {
-
-					$wrapper.css('height', 'auto');
-
-					window.setTimeout(function() {
-
-						var h = $wrapper.height(),
-							wh = $window.height();
-
-						if (h < wh)
-							$wrapper.css('height', '100vh');
-
-					}, 0);
-
-				});
-
-				$window.on('resize load', function() {
-					$window.triggerHandler('--refresh');
-				});
-
-			// Fix intro pic.
-				$('.panel.intro').each(function() {
-
-					var $pic = $(this).children('.pic'),
-						$img = $pic.children('img');
-
-					$pic
-						.css('background-image', 'url(' + $img.attr('src') + ')')
-						.css('background-size', 'cover')
-						.css('background-position', 'center');
-
-					$img
-						.css('visibility', 'hidden');
-
-				});
+			$panel = $panels.filter(window.location.hash);
+			$link = $nav_links.filter('[href="' + window.location.hash + '"]');
 
 		}
+
+		// No panel/link? Default to first.
+		if (!$panel
+			|| $panel.length == 0) {
+
+			$panel = $panels.first();
+			$link = $nav_links.first();
+
+		}
+
+		// Deactivate all panels except this one.
+		$panels.not($panel)
+			.addClass('inactive')
+			.hide();
+
+		// Activate link.
+		$link
+			.addClass('active');
+
+		// Reset scroll.
+		$window.scrollTop(0);
+
+		loadResume();
+	})();
+
+	// Hashchange event.
+	$window.on('hashchange', function (event) {
+
+		var $panel, $link;
+
+		// Get panel, link.
+		if (window.location.hash) {
+
+			$panel = $panels.filter(window.location.hash);
+			$link = $nav_links.filter('[href="' + window.location.hash + '"]');
+
+			// No target panel? Bail.
+			if ($panel.length == 0)
+				return;
+
+		}
+
+		// No panel/link? Default to first.
+		else {
+
+			$panel = $panels.first();
+			$link = $nav_links.first();
+
+		}
+
+		// Deactivate all panels.
+		$panels.addClass('inactive');
+
+		// Deactivate all links.
+		$nav_links.removeClass('active');
+
+		// Activate target link.
+		$link.addClass('active');
+
+		// Set max/min height.
+		$main
+			.css('max-height', $main.height() + 'px')
+			.css('min-height', $main.height() + 'px');
+
+		// Delay.
+		setTimeout(function () {
+
+			// Hide all panels.
+			$panels.hide();
+
+			// Show target panel.
+			$panel.show();
+
+			// Set new max/min height.
+			$main
+				.css('max-height', $panel.outerHeight() + 'px')
+				.css('min-height', $panel.outerHeight() + 'px');
+
+			// Reset scroll.
+			$window.scrollTop(0);
+
+			// Delay.
+			window.setTimeout(function () {
+
+				// Activate target panel.
+				$panel.removeClass('inactive');
+
+				// Clear max/min height.
+				$main
+					.css('max-height', '')
+					.css('min-height', '');
+
+				// IE: Refresh.
+				$window.triggerHandler('--refresh');
+
+				// Unlock.
+				locked = false;
+
+			}, (breakpoints.active('small') ? 0 : 500));
+
+		}, 250);
+
+		loadResume();
+	});
+
+	// IE: Fixes.
+	if (browser.name == 'ie') {
+
+		// Fix min-height/flexbox.
+		$window.on('--refresh', function () {
+
+			$wrapper.css('height', 'auto');
+
+			window.setTimeout(function () {
+
+				var h = $wrapper.height(),
+					wh = $window.height();
+
+				if (h < wh)
+					$wrapper.css('height', '100vh');
+
+			}, 0);
+
+		});
+
+		$window.on('resize load', function () {
+			$window.triggerHandler('--refresh');
+		});
+
+		// Fix intro pic.
+		$('.panel.intro').each(function () {
+
+			var $pic = $(this).children('.pic'),
+				$img = $pic.children('img');
+
+			$pic
+				.css('background-image', 'url(' + $img.attr('src') + ')')
+				.css('background-size', 'cover')
+				.css('background-position', 'center');
+
+			$img
+				.css('visibility', 'hidden');
+
+		});
+
+	}
 
 })(jQuery);
